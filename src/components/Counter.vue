f094c6bc374048ccbd28dddc327898cd9a4f8fc0
gloriadasilva <glotejuosho@gmail.com>
2025-05-24 08:45:47 +0100

    Added counter functionality

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,10 +1,12 @@
+<template>
+  <div class="counter">
+    <button @click="count++" class="btn">
+      Count Is: {{ count }}
+    </button>
+  </div>
+</template>
+
 <script setup>
 import { ref } from 'vue';
 const count = ref(0);
 </script>
-
-<template>
-  <button @click="count++">
-    Count is: {{ count }}
-  </button>
-</template>
