6ab020f4c5299e1d7925a4a0c0a189afb3056f08
gloriadasilva <glotejuosho@gmail.com>
2025-05-30 20:18:17 +0100

    Worked on the landing page

---
+++ src/components/cards.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="card">
+    <h3>Card Component</h3>
+    <p>Hi, I am a Vue component and can be reused multiple times!</p>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  border: 1px solid rgba(1, 135, 0, 0.4);
+  backdrop-filter: blur(4px);
+  border-radius: 12px;
+  padding: 1rem;
+  max-width: 320px;
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.05);
+  background-color: rgba(1, 135, 0, 0.8);
+
+}
+h2 {
+  color: #018700;
+  margin-bottom: 0.5rem;
+}
+</style>
